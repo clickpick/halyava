2eab4b75c571cba752d27e1bc031045349c2a2b0
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-02 18:00:11 +0400

    add api service

---
+++ src/services/api.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { parseQueryString } from 'helpers/location';
+import { getTimezoneOffset } from 'helpers/dates';
+
+const ENDPOINT = process.env.REACT_APP_API_URL;
+
+const instance = axios.create({
+    baseURL: ENDPOINT,
+    headers: {
+        'Vk-Params': window.btoa(JSON.stringify({
+            ...parseQueryString(window.location.search),
+            auth_type: 'front',
+            utc_offset: getTimezoneOffset()
+        })),
+        'Accept': 'application/json'
+    },
+});
+
+const get = (urn) => instance.get(urn);
+const post = (urn, body) => instance.post(urn, body);
+// const put = (urn, body = {}) => instance.put(urn, body);
+
+class API {
+    async auth() {
+        return await post('/vk-user/auth');
+    }
+
+    async map(topLeftLat, topLeftLng, botRightLat, botRightLng) {
+        const response = await get(`/vk-user/map?tllat=${topLeftLat}&tllng=${topLeftLng}&brlat=${botRightLat}&brlng=${botRightLng}`);
+
+        return response.data.data;
+    }
+}
+
+export default new API();
