1c8d594330e9a67d1dbb5d7e2dffcf334fdb2564
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-04 02:08:42 +0400

    change Home Panel

--- src/panels/Home/Home.js
+++ src/panels/Home/Home.js
@@ -1,80 +1,41 @@
-import React, { useEffect } from 'react';
-import { string } from 'prop-types';
+import React, { useCallback } from 'react';
+import { string, func } from 'prop-types';
 
 import './Home.css';
 
-import ymaps from 'ymaps';
-
-import API from 'services/api';
+import { useSelector, useDispatch } from 'react-redux';
+import { getMapState, getMapFeatures } from 'reducers/map-reducer';
+import { fetchFeatures } from 'actions/map-actions';
 
 import { Panel, PanelHeader } from '@vkontakte/vkui';
+import Map from 'components/Map';
 
-const Home = ({ id }) => {
+const Home = ({ id, goShop }) => {
+	const mapState = useSelector(getMapState);
+	const features = useSelector(getMapFeatures);
 
-	useEffect(() => {
-		ymaps
-			.load('https://api-maps.yandex.ru/2.1/?lang=ru_RU')
-			.then(async (maps) => {
-				const myMap = new maps.Map('map', {
-					center: [55.76, 37.64],
-					zoom: 5,
-					controls: ['zoomControl']
-				});
-				const MyIconContentLayout = maps.templateLayoutFactory.createClass(
-					'<div style="position: absolute; background-size: cover; box-shadow: 0 0 10px rgba(0,0,0,.3); background-image: url($[properties.iconContent]); width: 24px; height: 24px; top: -12px; left: -12px; border-radius: 100%; overflow: hidden;"></div>'
-				);
-				maps.layout.storage.add('myLayout', MyIconContentLayout);
+	const dispatch = useDispatch();
 
-				const yellowCollection = new maps.ObjectManager({ clusterize: true });
-
-				const bounds = myMap.getBounds();
-				const [topLeftLat, topLeftLng] = bounds[0];
-				const [botRightLat, botRightLng] = bounds[1];
-
-				myMap.geoObjects.add(yellowCollection);
-				
-				try {
-					const data = await API.map(topLeftLat, topLeftLng, botRightLat, botRightLng);
-					yellowCollection.add(data);
-				} catch (e) {}
-
-				function onObjectEvent(e) {
-					var objectId = e.get('objectId'),
-						objectGeometry = yellowCollection.objects.getById(objectId).geometry.type;
-					// Если событие произошло на метке, изменяем цвет ее иконки.
-					if (objectGeometry === 'Point') {
-						console.log(1);
-						yellowCollection.objects.setObjectOptions(objectId, {
-							preset: 'islands#yellowIcon'
-						});
-					}
-				}
-
-				yellowCollection.objects.events.add(['click'], onObjectEvent);
-
-				myMap.events.add('boundschange', async function () {
-					const bounds = myMap.getBounds();
-					const [topLeftLat, topLeftLng] = bounds[0];
-					const [botRightLat, botRightLng] = bounds[1];
-
-					try {
-						const data = await API.map(topLeftLat, topLeftLng, botRightLat, botRightLng);
-						yellowCollection.add(data);
-					} catch (e) { }
-				});
-			});
-	}, []);
-
+	const setFeatures = useCallback(function () {		
+		dispatch(fetchFeatures(...arguments));
+	}, [dispatch]);
+	
 	return (
 		<Panel id={id} className="Home">
 			<PanelHeader noShadow={true} />
-			<div id="map" className="Home__map" />
+			<Map
+				className="Home__Map"
+				mapState={mapState}
+				features={features}
+				fetchFeatures={setFeatures}
+				onClick={goShop} />
 		</Panel>
 	);
 };
 
 Home.propTypes = {
 	id: string.isRequired,
+	goShop: func.isRequired
 };
 
 export default Home;
