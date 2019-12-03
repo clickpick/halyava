36201a9d9f4981bc667405860d389e0731833eb6
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-04 02:07:41 +0400

    add Shop Panel in Main View

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { string } from 'prop-types';
 
 import useNavigation from 'hooks/use-navigation';
@@ -6,18 +6,26 @@ import useNavigation from 'hooks/use-navigation';
 import { ConfigProvider, View } from '@vkontakte/vkui';
 
 import Home from 'panels/Home';
+import Shop from 'panels/Shop';
 
 const Main = ({ id }) => {
-    const [activePanel, history] = useNavigation('home');
+    const [activePanel, history, goForward, goBack] = useNavigation('home');
+    const [shop, setShop] = useState(undefined);
+
+    const goShop = useCallback((shop) => {
+        setShop(shop);
+        goForward('shop');
+    }, [goForward]);
 
     return (
         <ConfigProvider isWebView={true}>
             <View
                 id={id}
                 activePanel={activePanel}
-                history={history}>
-                {/* onSwipeBack={goBack}> */}
-                <Home id="home" />
+                history={history}
+                onSwipeBack={goBack}>
+                <Home id="home" goShop={goShop} />
+                <Shop id="shop" shop={shop} goBack={goBack} />
             </View>
         </ConfigProvider>
     );
