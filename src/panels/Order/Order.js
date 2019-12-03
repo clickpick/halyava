7fb19682bf88213d60b08686fff85a2ab94e4f5f
Evgeniy Zavalishin <ezavalishin@gmail.com>
2019-12-04 03:06:11 +0400

    [WIP] order fetch

--- src/panels/Order/Order.js
+++ src/panels/Order/Order.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import { string, func } from 'prop-types';
+import React, {useState, useEffect} from 'react';
+import {func, string} from 'prop-types';
+import connect from '@vkontakte/vk-connect';
 
-import { Panel, PanelHeader, HeaderButton } from '@vkontakte/vkui';
+import {HeaderButton, Panel, PanelHeader, Button} from '@vkontakte/vkui';
 
-const Order = ({ id, orderId, goMain }) => {
+import API from 'services/api';
+
+const Order = ({id, orderId, goMain}) => {
+
+    const [order, setOrder] = useState({});
+
+    async function openVkPay() {
+        const response = await API.vkPayParams(orderId);
+
+        connect.sendPromise('VKWebAppOpenPayForm', response).then((response) => {
+
+        }).catch((e) => console.log(e));
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await API.order(orderId);
+            setOrder(response);
+        }
+        fetchData();
+    }, [orderId]);
 
     return (
         <Panel id={id}>
             <PanelHeader
-                left={<HeaderButton children="Закрыть" onClick={goMain} />}
+                left={<HeaderButton children="Закрыть" onClick={goMain}/>}
                 children="Оплата заказа"
-                noShadow={true} />
+                noShadow={true}/>
 
             <p>Order ID: {orderId}</p>
+            <Button onClick={() => openVkPay()}>{order.id}</Button>
         </Panel>
     );
 };
