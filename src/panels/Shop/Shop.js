6cc9c767f8aa5c1df69c3bff718c6f4ea8788f24
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-05 03:51:46 +0400

    add info in Shop Panel

--- src/panels/Shop/Shop.js
+++ src/panels/Shop/Shop.js
@@ -1,16 +1,100 @@
-import React from 'react';
-import { string, func } from 'prop-types';
+import React, { useState, useCallback, useEffect } from 'react';
+import { string, shape, func } from 'prop-types';
 
 import './Shop.css';
 
+import connect from '@vkontakte/vk-connect';
+import { VK_APP_ID } from 'constants/vk';
+import { useDispatch } from 'react-redux';
+import { showPopup } from 'actions/popup-actions';
+import * as POPUP from 'constants/popup';
+
 import { Panel, PanelHeader, HeaderButton, platform, IOS } from '@vkontakte/vkui';
+import Wrapper from 'components/Wrapper';
+import ShopCard from 'components/ShopCard';
+import Loader from 'components/Loader';
+import Gallery from 'components/Gallery';
+import { InfoRow, FixedLayout } from '@vkontakte/vkui';
+import Accordion from 'components/Accordion';
+import Link from 'components/Link';
+import Button from 'components/Button';
 
 import Icon24Back from '@vkontakte/icons/dist/24/back';
 import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import { ReactComponent as IconVk } from 'svg/vk.svg';
 
 const isIOS = platform() === IOS;
 
-const Shop = ({ id, goBack }) => {
+const Shop = ({ id, shop, goBack }) => {
+    const [loading, setLoading] = useState(false);
+    const [photos, setPhotos] = useState([]);
+    const [currentAddress, setCurrentAddress] = useState(null);
+    const [addresses, setAddresses] = useState([]);
+    const [group, setGroup] = useState(null);
+
+    const dispatch = useDispatch();
+
+    const renderAddress = useCallback((item) =>
+        <Link href="#" children={item.address} icon="point" />, []);
+
+    useEffect(() => {
+        async function fetchInfo() {
+            setLoading(true);
+
+            try {
+                const { access_token } = await connect.sendPromise('VKWebAppGetAuthToken', {
+                    app_id: VK_APP_ID,
+                    scope: ''
+                });
+
+                const v = '5.103';
+
+                const { response: { items: photos } } = await connect.sendPromise('VKWebAppCallAPIMethod', {
+                    method: 'photos.get',
+                    params: {
+                        owner_id: `-${shop.properties.group.id}`,
+                        album_id: shop.properties.group.album_id || 'wall',
+                        count: 20,
+                        access_token,
+                        v
+                    }
+                });
+
+                const { response: { items: addresses } } = await connect.sendPromise('VKWebAppCallAPIMethod', {
+                    method: 'groups.getAddresses',
+                    params: {
+                        group_id: shop.properties.group.id,
+                        access_token,
+                        v
+                    }
+                });
+                
+
+                const { response: [group] } = await connect.sendPromise('VKWebAppCallAPIMethod', {
+                    method: 'groups.getById',
+                    params: {
+                        group_id: shop.properties.group.id,
+                        fields: ['can_message', 'description', 'place', 'site'],
+                        access_token,
+                        v
+                    }
+                });                
+
+                setPhotos(photos);
+                setCurrentAddress(addresses.find((address) => address.id === shop.id));
+                setAddresses(addresses.filter((address) => address.id !== shop.id));
+                setGroup(group);
+            } catch (e) {
+                if (e.error_data.error_code === 4) {
+                    dispatch(showPopup(POPUP.TOKEN_DENIED));
+                }
+            }
+
+            setLoading(false);
+        }
+
+        fetchInfo();
+    }, [dispatch, shop]);
 
     return (
         <Panel id={id} className="Shop">
@@ -22,12 +106,89 @@ const Shop = ({ id, goBack }) => {
                         Карта
                     </HeaderButton>
                 } />
+
+            <Wrapper className="Shop__Wrapper">
+                <ShopCard
+                    className="Shop__ShopCard"
+                    name={shop.properties.group.name}
+                    activity={shop.properties.group.activity}
+                    photo={shop.properties.iconContent}
+                    cashback={shop.properties.group.cashback_value} />
+
+                {(loading)
+                    ? <Loader className="Shop__Loader" center />
+                    : <>
+                        {(photos && photos.length > 0) &&
+                            <Gallery className="Shop__Gallery" photos={photos} />}
+
+                        {(currentAddress) && <>
+                            {/* расписание */}
+
+                            {(currentAddress.address) &&
+                                <InfoRow
+                                    className="Shop__InfoRow"
+                                    title="Адрес"
+                                    children={currentAddress.address} />}
+
+                            {(addresses && addresses.length > 0) &&
+                                <Accordion
+                                    className="Shop__Accordion"
+                                    title="Филиалы"
+                                    items={addresses.map(renderAddress)} />}
+
+                            {(currentAddress.phone) &&
+                                <InfoRow className="Shop__InfoRow" title="Номер телефона">
+                                    <a href={`tel:${currentAddress.phone}`} children={currentAddress.phone} />
+                                </InfoRow>}
+                        </>}
+
+                        {(group) && <>
+                        
+                            {(group.site) &&
+                                <InfoRow className="Shop__InfoRow" title="Сайт">
+                                    <a
+                                        href={group.site}
+                                        children={group.site}
+                                        target="_blank"
+                                        rel="noopener noreferrer" />
+                                </InfoRow>}
+
+                            {(group.description) &&
+                                <InfoRow
+                                    className="Shop__InfoRow"
+                                    title="Описание из группы"
+                                    children={group.description} />}
+                        </>}
+
+                        <FixedLayout
+                            className="Shop__FixedLayout"
+                            vertical="bottom">
+                            <Button
+                                className="Shop__Button"
+                                theme="primary"
+                                size="medium"
+                                children="Написать сообщение"
+                                before={<IconVk />}
+                                full
+                                backlight />
+                        </FixedLayout>
+                    </>}
+            </Wrapper>
         </Panel>
     );
 };
 
 Shop.propTypes = {
     id: string.isRequired,
+    shop: shape({
+        properties: shape({
+            group: shape({
+                name: string,
+                activity: string
+            }),
+            iconContent: string
+        })
+    }),
     goBack: func.isRequired
 };
 
