f993fbcd224189f8530a5c33943ff92f4d4c478f
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-04 22:32:52 +0400

    add Popup Component

---
+++ src/components/Popup/Dialog.jsx
@@ -0,0 +1,176 @@
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { string, oneOf, arrayOf, shape, bool, func } from 'prop-types';
+import classNames from 'classnames';
+
+import './Dialog.css';
+
+import Button from 'components/Button';
+
+import { useSwipeable, UP, DOWN } from 'react-swipeable';
+import useLockBody from 'hooks/use-lock-body';
+
+const Dialog = ({ className, disabled, onClose, animationType, type, title, message, children, actions }) => {
+    useLockBody(true);
+
+    const wrapperRef = useRef();
+
+    const [top, setTop] = useState(0);
+    const [hasScroll, setHasScroll] = useState(false);
+    const [scrolling, setScrolling] = useState(false);
+    const [dragging, setDragging] = useState(false);
+
+    const initialWrapper = useCallback(() => {
+        if (wrapperRef && wrapperRef.current) {
+            const { scrollHeight, offsetHeight } = wrapperRef.current;
+
+            setHasScroll(scrollHeight > offsetHeight);
+        }
+    }, [wrapperRef]);
+
+    useEffect(() => {
+        initialWrapper();
+
+        window.addEventListener('resize', initialWrapper);
+
+        return () => {
+            window.removeEventListener('resize', initialWrapper);
+        };
+    }, [initialWrapper]);
+
+    function handleSwiping({ deltaY, event, dir }) {
+        if (disabled) {
+            return;
+        }
+
+        const target = event.target;
+        const wrapper = wrapperRef.current;
+
+        if (target && wrapper) {
+            const scrolled = wrapper.scrollTop === wrapper.scrollHeight - wrapper.offsetHeight;
+            const scrollTop = dir === UP;
+            const scrollDown = dir === DOWN;
+
+            if (hasScroll && !target.classList.contains('Dialog__footer')) {
+                setScrolling(true);
+
+                if (scrollTop) {
+                    // eslint-disable-next-line no-mixed-operators
+                    if (!scrolled || scrolling && !dragging) {
+                        return;
+                    }
+                    
+                    setScrolling(false);
+                }
+
+                if (scrollDown) {
+                    // eslint-disable-next-line no-mixed-operators
+                    if (scrolled || scrolling && !dragging) {
+                        return;
+                    }
+                }
+            }
+        }
+
+        if (!scrolling) {
+            event.preventDefault();
+
+            if (!dragging) {
+                setDragging(true);
+            }
+
+            if (deltaY > 0) {
+                setTop(deltaY * (-1));
+            }
+        }
+    }
+
+    function handleSwipedUp() {
+        if (scrolling) {
+            setScrolling(false);
+        }
+
+        if (dragging) {
+            setDragging(false);
+
+            if (top < -50) {
+                onClose();
+
+                return;
+            }
+
+            const timerId = setInterval(() => {
+                setTop((top) => {
+                    if (top >= 0) {
+                        clearInterval(timerId);
+                        return 0;
+                    }
+
+                    return top + 1;
+                });
+            }, 1);
+        }
+    }
+
+    const handlers = useSwipeable({
+        onSwiping: handleSwiping,
+        onSwipedUp: handleSwipedUp,
+        preventDefaultTouchmoveEvent: false,
+        trackMouse: true
+    });
+    
+    const handleClick = useCallback((e) => e.stopPropagation(), []);
+
+    function renderAction(action, index) {
+        return <Button
+            key={index}
+            className="Dialog__action"
+            theme={action.theme}
+            size="medium"
+            children={action.title}
+            full={action.full}
+            onClick={action.action}
+            disabled={action.disabled} />;
+    }
+
+    return (
+        <div
+            className={classNames(
+                className,
+                'Dialog',
+                `Dialog--${type}`,
+                `Dialog--slide-down-${animationType}`
+            )}
+            onClick={handleClick}
+            {...handlers}
+            style={{ top: `${top}px` }}>
+            <div className="Dialog__wrapper" ref={wrapperRef}>
+                <h3 className="Dialog__title" children={title} />
+                {message && <p className="Dialog__message" dangerouslySetInnerHTML={{ __html: message }} />}
+                {children}
+
+                {(Array.isArray(actions) && actions.length > 0) &&
+                    <div className="Dialog__actions" children={actions.map(renderAction)} />}
+            </div>
+        </div>
+    );
+};
+
+Dialog.propTypes = {
+    className: string,
+    animationType: oneOf(['enter', 'leave']).isRequired,
+    type: oneOf(['info', 'success', 'danger']),
+    title: string.isRequired,
+    message: string,
+    actions: arrayOf(shape({
+        theme: oneOf(['primary', 'secondary', 'info', 'link']),
+        title: string,
+        action: func,
+        full: bool
+    }))
+};
+
+Dialog.defaultProps = {
+    type: 'info',
+};
+
+export default Dialog;
---
+++ src/components/Popup/Popup.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { string, bool, func, oneOf, arrayOf, shape } from 'prop-types';
+import classNames from 'classnames';
+
+import './Popup.css';
+
+import Dialog from './Dialog';
+
+const Popup = ({ className, visible, disabled, onClose, type, title, message, children, actions }) => {
+    const [show, setShow] = useState(false);
+    const [animationType, setAnimationType] = useState('leave');
+
+    useEffect(() => {
+        if (visible && !show) {
+            setShow(true);
+            setAnimationType('enter');
+        } else if (show && !visible) {
+            setAnimationType('leave');
+        }
+    }, [visible, show]);
+
+    const style = {
+        display: (show) ? '' : 'none',
+    };
+
+    function animationEnd() {
+        if (animationType === 'leave') {
+            setShow(false);
+        }
+    }
+
+    function close() {
+        if (!disabled && onClose) {
+            onClose();
+        }
+    }
+
+    return (
+        <div
+            style={style}
+            className={classNames(className, 'Popup', `Popup--fade-${animationType}`)}
+            tabIndex={-1}            
+            onClick={close}
+            onAnimationEnd={animationEnd}>
+            <div className="Popup__mask" />
+            {(show) && <Dialog
+                className="Popup__Dialog"
+                disabled={disabled}
+                onClose={close}
+                animationType={animationType}
+                type={type}
+                title={title}
+                message={message}
+                children={children}
+                actions={actions} />}
+        </div>
+    );
+};
+
+Popup.propTypes = {
+    className: string,
+    visible: bool,
+    disabled: bool,
+    type: oneOf(['info', 'success', 'danger']),
+    title: string.isRequired,
+    message: string,
+    onClose: func,
+    actions: arrayOf(shape({
+        theme: oneOf(['primary', 'secondary', 'info', 'link']),
+        title: string,
+        action: func,
+        full: bool
+    }))
+};
+
+export default Popup;
