0a94c2ee6437147caa6b0c9d6a917e3733482eaa
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-06 02:35:20 +0400

    add AddReviewFrom Component

---
+++ src/components/AddReviewForm/AddReviewForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState, useCallback } from 'react';
+import { string, func } from 'prop-types';
+import classNames from 'classnames';
+
+import './AddReviewForm.css';
+
+import { FormLayout, Textarea } from '@vkontakte/vkui';
+import Button from 'components/Button';
+
+const AddReviewForm = ({ className, onSubmit }) => {
+    const [review, setReview] = useState('');
+    const [loading, setLoading] = useState(false);
+
+    const handleChange = useCallback((e) => setReview(e.target.value), []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const text = review.trim();
+
+        if (!text) {
+            return;
+        }
+
+        if (onSubmit) {
+            setLoading(true);
+            onSubmit(text);
+        }
+    };
+
+    return (
+        <FormLayout className={classNames(className, 'AddReviewForm')} onSubmit={handleSubmit}>
+            <Textarea
+                className="AddReviewForm__Textarea"
+                top="Твой отзыв"
+                placeholder="Напиши здесь что думаешь об этом месте"
+                value={review}
+                onChange={handleChange} />
+            <Button
+                type="submit"
+                className="AddReviewForm__Button"
+                theme="primary"
+                size="medium"
+                children="Отправить"
+                disabled={loading}
+                full
+                backlight />
+        </FormLayout>
+    );
+};
+
+AddReviewForm.propTypes = {
+    className: string,
+    onSubmit: func
+};
+
+export default AddReviewForm;
