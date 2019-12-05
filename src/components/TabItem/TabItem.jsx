d74ea3a0baafe13a40f4cc5c4a758d276dd7523b
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-06 01:01:44 +0400

    add TabItem Component

---
+++ src/components/TabItem/TabItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { string, bool, func } from 'prop-types';
+import classNames from 'classnames';
+
+import './TabItem.css';
+
+const TabItem = ({ className, index, title, active, onClick }) =>
+    <button
+        className={classNames(className, 'TabItem')}
+        aria-pressed={active}
+        children={title}
+        data-index={index}
+        onClick={onClick} />;
+
+TabItem.propTypes = {
+    className: string,
+    index: string.isRequired,
+    title: string.isRequired,
+    active: bool,
+    onClick: func
+};
+
+export default TabItem;
