e8212479e68abc2ca17e3100aba0194ceccfac32
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-06 22:51:18 +0400

    add view prop for Loader Component

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,27 +1,37 @@
 import React from 'react';
-import { string, bool } from 'prop-types';
+import { string, bool, oneOf } from 'prop-types';
 import classNames from 'classnames';
 
 import './Loader.css';
 
 import { Spinner } from '@vkontakte/vkui';
 
-const Loader = ({ className, center, absoluteCenter, ...restProps }) => {
+import { ReactComponent as IconInvoice } from 'svg/invoice-skeleton.svg';
+
+const Loader = ({ className, view, center, absoluteCenter, ...restProps }) => {
     const props = {
         className: classNames(className, 'Loader', {
+            [`Loader--${view}`]: view,
             'Loader--is-center': center,
             'Loader--is-absolute-center': absoluteCenter,
         }),
         ...restProps,
     };
 
-    return <Spinner {...props} />;
+    return (view === 'spinner')
+        ? <Spinner {...props} />
+        : <IconInvoice {...props} />;
 };
 
 Loader.propTypes = {
     className: string,
+    view: oneOf(['spinner', 'invoice']),
     center: bool,
-    absoluteCenter: bool,
+    absoluteCenter: bool
+};
+
+Loader.defaultProps = {
+    view: 'spinner'
 };
 
 export default Loader;
