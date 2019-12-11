059bad0575c04fe290a3c1d77b950fe96f00ead3
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-12 00:43:52 +0400

    add css vars for Map Component

--- src/components/MapProvider/MapProvider.jsx
+++ src/components/MapProvider/MapProvider.jsx
@@ -13,7 +13,7 @@ const MapProvider = React.memo(({ children, ymaps }) => {
     useEffect(() => {
         if (!hasLayout) {
             const MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
-                '<div style="position: absolute; background-size: cover; box-shadow: 0 0 10px rgba(0,0,0,.3); background-image: url($[properties.iconContent]); width: 24px; height: 24px; top: -12px; left: -12px; border-radius: 100%; overflow: hidden;"></div>'
+                '<div class="Map__Icon" style="background-image: url($[properties.iconContent]);"></div>'
             );
             ymaps.layout.storage.add('myLayout', MyIconContentLayout);
 
