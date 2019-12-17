846bd757a994b3048b3829917f92f860fea2c909
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-18 00:48:57 +0400

    change ReviewsList Component

--- src/components/ReviewsList/ReviewsList.jsx
+++ src/components/ReviewsList/ReviewsList.jsx
@@ -1,12 +1,18 @@
 import React, { useCallback } from 'react';
-import { string, arrayOf, shape, number } from 'prop-types';
+import { string, arrayOf, shape, number, func } from 'prop-types';
 import classNames from 'classnames';
 
 import Review from 'components/Review';
 
-const ReviewsList = ({ className, reviews }) => {
+const ReviewsList = ({ className, reviews, userId, onRemove }) => {
     const renderReview = useCallback((review, index) =>
-        <Review key={index} className="ReviewsList__Review" {...review} />, []);
+        <Review
+            key={index}
+            className="ReviewsList__Review"
+            userId={userId}
+            onRemove={onRemove}
+            {...review} />,
+        [userId, onRemove]);
 
     return <div className={classNames(className, 'ReviewsList')} children={reviews.map(renderReview)} />;
 };
@@ -21,7 +27,9 @@ ReviewsList.propTypes = {
             first_name: string.isRequired,
             last_name: string.isRequired
         }).isRequired,
-    }))
+    })),
+    userId: number,
+    onRemove: func
 };
 
 export default ReviewsList;
