cc6bb132ddcac105e423c32dc7c541ec6d09dfb9
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-18 02:35:23 +0400

    add declOfNumber helper

--- src/helpers/numbers.js
+++ src/helpers/numbers.js
@@ -35,4 +35,9 @@ export const stringToNumber = (str) => {
     return str ? parseFloat(str.replace(/(\s)|(₽)|(,)/g, '')) || 0 : 0;
 };
 
+export const declOfNumber = (number, titles) => {
+    const cases = [2, 0, 1, 1, 1, 2];
+    return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+};
+
 export { format as numberToString, format as gaps };
