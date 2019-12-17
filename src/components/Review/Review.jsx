5dcd9ab15c8ce5211096e0c8f6ed006b8449c2ba
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-18 00:48:44 +0400

    change Reivew Component

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { string, shape, number } from 'prop-types';
+import { string, shape, number, oneOf, func } from 'prop-types';
 import classNames from 'classnames';
 
 import './Review.css';
 
 import { Avatar } from '@vkontakte/vkui';
+import Tag from 'components/Tag';
 
-const Review = ({ className, text, user }) => {
-    const name = `${user.first_name} ${user.last_name}`;
+import { ReactComponent as IconLike } from 'svg/like.svg';
+
+const MARK_LIKE = 'like';
+const MARK_DISLIKE = 'dislike';
+
+const Review = React.memo(({ className, id, text, mark, user, userId, onRemove }) => {
+    const isCurrentUserReview = user.id === userId;
+    const name = `${user.first_name} ${user.last_name} `;
+
+    let tagText = 'Нравится';
+    let tagTheme = 'green';
+    let iconLikeClassName = 'Review__IconLike';
+    if (mark === MARK_DISLIKE) {
+        tagText = 'Не нравится';
+        tagTheme = 'red';
+        iconLikeClassName += '  Review__IconLike--dislike';
+    }
+
+    function handleRemoveClick() {        
+        if (onRemove) {
+            onRemove(id);
+        }
+    }
 
     return (
         <div className={classNames(className, 'Review')}>
@@ -21,23 +43,40 @@ const Review = ({ className, text, user }) => {
                     className="Review__name"
                     href={`https://vk.com/id${user.id}`}
                     target="_blank"
-                    rel="noopener noreferrer"
-                    children={name} />
+                    rel="noopener noreferrer">
+                    {name}
+                    {(isCurrentUserReview) && <span className="Review__user-mark" children="(вы)" />}
+                </a>
                 <p className="Review__text" children={text} />
+
+                <footer className="Review__footer">
+                    <Tag className="Review__Tag" theme={tagTheme}>
+                        <IconLike className={iconLikeClassName} />{tagText}
+                    </Tag>
+                    {(isCurrentUserReview) &&
+                        <button
+                            className="Review__remove-review"
+                            children="Удалить отзыв"
+                            onClick={handleRemoveClick} />}
+                </footer>
             </div>
         </div>
     );
-};
+});
 
 Review.propTypes = {
     className: string,
+    id: number.isRequired,
     text: string.isRequired,
+    mark: oneOf([MARK_LIKE, MARK_DISLIKE]).isRequired,
     user: shape({
         id: number.isRequired,
         avatar_200: string,
         first_name: string.isRequired,
         last_name: string.isRequired
-    }).isRequired
+    }).isRequired,
+    userId: number,
+    onRemove: func
 };
 
 export default Review;
