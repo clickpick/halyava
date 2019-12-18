1ec19ac4409ee7067141409316747a36053321f3
Nikita Barinov <nikitabarinov2301@gmail.com>
2019-12-18 19:49:07 +0400

    remove minZoom in Map Component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,7 +5,7 @@ import './Map.css';
 import { Map as YMap, ObjectManager, Placemark, YMaps } from 'react-yandex-maps';
 import MapProvider from 'components/MapProvider';
 
-const OPTIONS = { minZoom: 10, suppressMapOpenBlock: true };
+const OPTIONS = { suppressMapOpenBlock: true }; // minZoom: 10
 const PLACEMARK_OPTIONS = { preset: 'islands#geolocationIcon' };
 
 const Map = ({ mapState, userGeometry, features, fetchFeatures, updateMapState, onClick }) => {
